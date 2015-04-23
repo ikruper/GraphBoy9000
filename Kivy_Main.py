95f7d9faf029454582850ac5363feecd33e0db9a
Ian Kruper <ikruper@ycp.edu>
2015-04-23 14:18:57 -0400

    Big commit..sorry. Most features implemented.

--- Kivy_Main.py
+++ Kivy_Main.py
@@ -6,14 +6,64 @@ Created on Thu Mar 26 15:28:50 2015
 """
 
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+#import kivy.base as Base
+import pylab
+import Line_Conditioning
 
-class Menu(Widget):
-    pass
+class Menu(GridLayout):
+    x_vals = []
+    y_vals = []
+    lbf = ''
+    rsq = ''
+    val_counter = 0
+    ser_counter = 0
+    def get_current_value(self, *args):
+        if (self.ids['x_value'].text != None and self.ids['y_value'].text != None):
+            self.x_vals.append(float(self.ids['x_value'].text))
+            self.y_vals.append(float(self.ids['y_value'].text))
+            self.ids['x_value'].text = ""
+            self.ids['y_value'].text = ""
+            self.val_counter += 1
+            self.ids['current_value'].text = "Value "+str(self.val_counter + 1)
+    
+    def new_series(self, *args):
+        if (self.ser_counter < 5):
+            if (self.ids['dots_radio'].active == True):
+                pylab.plot(self.x_vals, self.y_vals, 'o', label=self.ids['data_series_label'].text)
+            elif (self.ids['line_radio'].active == True):
+                pylab.plot(self.x_vals, self.y_vals, label=self.ids['data_series_label'].text)
+            Line_Conditioning.Padding(self.x_vals, self.y_vals)
+            self.x_vals = []
+            self.y_vals = []
+            self.ser_counter += 1
+            self.val_counter = 1
+            self.ids['data_series_label'].text = ""
+            self.ids['current_value'].text = "Value "+str(self.val_counter)
+            self.ids['x_value'].text = ""
+            self.ids['y_value'].text = ""
+            self.ids['lbf_check'].active == False
+    
+    def submit(self, *args):
+        pylab.title(self.ids['graph_title'].text)
+        pylab.xlabel(self.ids['xaxis_label'].text)
+        pylab.ylabel(self.ids['yaxis_label'].text)
+        pylab.grid(b=True, which="both")
+        if (self.ids['dots_radio'].active == True):
+            pylab.plot(self.x_vals, self.y_vals, 'o', label=self.ids['data_series_label'].text)
+        elif (self.ids['line_radio'].active == True):
+           pylab.plot(self.x_vals, self.y_vals, label=self.ids['data_series_label'].text)
+        if (self.ids['lbf_check'].active == True and self.ser_counter == 0):
+            lbf, rsq = Line_Conditioning.DrawLine(self.x_vals, self.y_vals)
+            self.ids['lbf_equation'].text = lbf
+            self.ids['r_squared'].text = rsq
+        pylab.legend(loc='best')
+        pylab.show()
 
 class GraphBoy9000App(App):
     def build(self):
-        return Menu()
+        menu = Menu()
+        return menu
 
 if __name__ == "__main__":
     GraphBoy9000App().run()
---
+++ Line_Conditioning.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 10:10:58 2015
+
+@author: ian
+"""
+
+import numpy
+import pylab
+from scipy import stats
+
+def DrawLine(x_values, y_values):
+    lbf_x_values = numpy.array(x_values)
+    lbf_y_values = numpy.array(y_values)
+    lbf_values = numpy.polyfit(lbf_x_values, lbf_y_values, 1)
+    pylab.plot([x_values[0], x_values[-1]], [lbf_values[1]+
+                lbf_values[0]*x_values[0], 
+                lbf_values[1]+lbf_values[0]*x_values[-1]], 'k--')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(lbf_x_values
+                ,lbf_y_values)
+    if (slope == 0):
+        best_fit_line = 'y = '+str(intercept)
+    elif (intercept == 0):
+        best_fit_line = 'y = '+str(slope)+'x'
+    elif (intercept < 0):
+        best_fit_line = 'y = '+(str(slope)+'x - '+str(abs(intercept)))
+    else:
+        best_fit_line = 'y = '+(str(slope)+'x + '+str(intercept))
+    r_squared = str(r_value**2)
+    return best_fit_line, r_squared
+    
+def Padding(x_values, y_values):
+#    pylab.plot(x_values[len(x_values)-1]+(0.2*(x_values[len(x_values)-1]-x_values[0]))
+#                , y_values[0], visible=False)
+    pylab.plot(x_values[len(x_values)-1]+(0.2*(x_values[len(x_values)-1]-x_values[0])),
+                        y_values[len(y_values)-1]+(0.2*(y_values[len(y_values)-1]-y_values[0])),
+                        x_values[0]-(0.2*(x_values[len(x_values)-1]-x_values[0])),
+                        y_values[0]-(0.2*(y_values[len(y_values)-1]-y_values[0])), visible=False)
